cf65a5dd71e35a3a8bfaf90ba26b5125cc0ccd26
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-02-11 11:45:01 -0600

    completed functions to build options arrays and generate a password

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,62 +1,88 @@
+
 var generateBtn = document.querySelector("#generate");
 
+let passlen = '';
+let password = ''
+let userChoices = [];
+let finalOptions = [];
 
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+let numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '/', ':', ';', "'", '"', '<', '>', ',', '.', '?'];
 
-function generatePassword() {
+let possibleOptions = [lowerCase, upperCase, numeric, specialChar];
 
-    let passLen = prompt("Choose length between 8 and 128 characters");
-    //change to where null will not force the repeat of the prompt so that the ccancel button actually works
-    while ( passLen === null ||isNaN(passLen) || passLen < 8 || passLen > 128) {
-        window.alert("Please enter a valid response");
-        passLen = prompt("Please enter a numeric value between 8 and 128.");  
-    };
+//determines the password length
+function passwordLength() {
 
-    let includeLower = prompt("Include lowercase? y/n").toLowerCase().split('', 1).toString();
-    while (includeLower != "y" || includeLower != "n") {
-        window.alert("Please enter  valid response.\n yes or no");
-        includeLower = prompt("Include lowercase? y/n").toLowerCase().split('', 1).toString();
-        // console.log(includeLower); conole.log shows it being y or n
-    };   
+    while (true) {
+        passlen = prompt("Enter desired password length (8 - 128)");
 
-    let includeUpper = prompt("Include uppercase? y/n").toLowerCase().split('', 1).toString();
-    console.log(includeUpper);
-    while (includeUpper != "y" || includeUpper != "n") {
-        window.alert("Please enter  valid response.\n yes or no");
-        includeLower = prompt("Include lowercase? y/n").toLowerCase().split('', 1).toString();
-        console.log(includeUpper);
-    }; 
+        if (!passlen || isNaN(passlen) || passlen < 8 || passlen > 128) {
+            window.alert("Please enter a valid input betwen 8 - 128.");
+        } else {
+            console.log(passlen);
+            return;
+            //if the cancel button is hit it re prompts the question instead of quitting out like the charChoices function would
+        }
+    }
+}
 
-    let includeNumberic = prompt("Include numbers? y/n").toLowerCase().split('', 1).toString();
-    while (includeUpper != "y" || includeUpper != "n") {
-        window.alert("Please enter  valid response.\n yes or no");
-        includeLower = prompt("Include lowercase? y/n").toLowerCase().split('', 1).toString();
-        console.log(includeUpper);
-    }; 
-
-    let includeSpecial = prompt("Include special characters? y/n").toLowerCase().split('', 1).toString();
-    while (includeUpper != "y" || includeUpper != "n") {
-        window.alert("Please enter  valid response.\n yes or no");
-        includeLower = prompt("Include lowercase? y/n").toLowerCase().split('', 1).toString();
-        console.log(includeUpper);
-    }; 
-
-    // function checkLen() {
-        
+//builds array of user responses to the prompt questions
+function charChoices() {
+    let questions = [["Include lowercase letters? Please answer yes or no"], ["Include uppercase letters? Please answer yes or no"], ["Include numbers? Please answer yes or no"], ["Include special characters? Please answer yes or no"]];
+    // while (true) {
+        for (let i =0; i < questions.length; i++) {
+            let yesOrNo = prompt(questions[i]).toLowerCase();
+            if (yesOrNo !== 'yes' && yesOrNo !== 'no') {
+                window.alert('Please enter a valid respons of yes or no.')
+            } else {
+                userChoices.push(yesOrNo);
+                console.log(userChoices);
+            }
+        }
     // }
+    //without the while statemnet, even if the answer isnt valid and it re prompts it goes to the next question, with while statement, it just keeps asking the questions over and over non stop
+}
 
-        
+//builds he array of the final options of characters for the generated password based on prompt responses
+function optionsIncluded() {
+    for (let i = 0; i < userChoices.length; i++){
+        if (userChoices[i] == 'yes') {
+            finalOptions += possibleOptions[i];
+            console.log(finalOptions);
+            //the last character from each option set isnt seperated from the first character of the next set when it gets added to the final options array
+        }
+    }
+}
 
-
+//builds the new password
+function generatePassword() {
+    for (i = 0; i < passlen; i++) {
+        let charToAdd = finalOptions[Math.floor(Math.random() * finalOptions.length)];
+        password += charToAdd;
+    }
 };
 
+function resetValues() {
+    passlen = '';
+    password = ''
+    userChoices = [];
+    finalOptions = [];
+}
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
+    passwordLength();
+    charChoices()
+    optionsIncluded();
+    generatePassword();
 
     passwordText.value = password;
 
+    resetValues();
 }
 
 // Add event listener to generate button
